a55ae1b56db8ae7aedd948bc3c9f62ff7cadb509
Ivditi Gabeskiria <ivg4001@med.cornell.edu>
2019-07-17 22:52:30 -0400

    add imagepicker

--- PhotoEditor/ViewController.swift
+++ PhotoEditor/ViewController.swift
@@ -6,14 +6,40 @@
 //
 
 import UIKit
+import CoreImage
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UINavigationControllerDelegate, UIImagePickerControllerDelegate {
 
+    
+    @IBOutlet weak var imageView: UIImageView!
+    
+     let imagePicker = UIImagePickerController()
+    
+    @IBAction func uploadImageButtonTapped(_ sender: Any) {
+        imagePicker.allowsEditing = false
+        imagePicker.sourceType = .photoLibrary
+        
+        present(imagePicker, animated: true, completion: nil)
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        imagePicker.delegate = self
+    }
+    
+
+
+    // MARK: - UIImagePickerControllerDelegate Methods
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        if let pickedImage = info[UIImagePickerController.InfoKey.originalImage] as? UIImage {
+            imageView.contentMode = .scaleAspectFit
+            imageView.image = pickedImage
+        }
+
+        dismiss(animated: true, completion: nil)
     }
 
-
+    
 }
 
